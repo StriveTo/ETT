85f522f566fdd709dfeda00519ac2cbf608dc512
liuabo <ln960523@163.com>
2018-01-29 14:20:22 +0800

    Add a list header

--- ttgy/src/App.vue
+++ ttgy/src/App.vue
@@ -1,23 +1,28 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+  	<common-header></common-header>
     <router-view/>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+
+  }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+	html,body{
+		height:100%;
+	}
+#app{
+	width:100%;
+	height:100%;
+	display:flex;
+	flex-direction:column;
+	justify-content:space-between;
 }
 </style>
--- ttgy/src/main.js
+++ ttgy/src/main.js
@@ -2,14 +2,25 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import store from './vuex'
 import router from './router'
+
+import Header_lb from './components/Header_lb'
+
+import './assets/css/reset.css'
+import './assets/iconfont/iconfont.css'
+import './assets/css/swiper.min.css'
 
 Vue.config.productionTip = false
 
+
+// 全局注册组件
+Vue.component("common-header", Header_lb);
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
