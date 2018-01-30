3a16da8bc2c538a9856f1c51eb7a59397b0586c6
lpz10 <li2012670869@163.com>
2018-01-30 18:04:45 +0800

    change_log

--- ttgy/src/App.vue
+++ ttgy/src/App.vue
@@ -1,9 +1,5 @@
 <template>
   <div id="app">
-<<<<<<< HEAD
-=======
-  	<common-header></common-header>
->>>>>>> origin/zll
     <router-view/>
   </div>
 </template>
@@ -12,26 +8,12 @@
 export default {
   name: 'App',
   components: {
-
+		
   }
 }
 </script>
 
-<<<<<<< HEAD
 <style>
 	@import "./components/css/reset.css";
 	@import "iconfont/iconfont.css";
-=======
-<style lang="scss">
-	html,body{
-		height:100%;
-	}
-#app{
-	width:100%;
-	height:100%;
-	display:flex;
-	flex-direction:column;
-	justify-content: space-between;
-}
->>>>>>> origin/zll
 </style>
--- ttgy/src/router/index.js
+++ ttgy/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import lb1 from '@/components/lb1'
+import log from '@/components/log'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/lb1',
       name: 'lb1',
       component: lb1
+    },
+     {
+      path: '/log',
+      name: 'log',
+      component: log
     }
   ]
 })
