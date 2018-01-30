1987bbcd04ccc08e24e146c89a5e2a50af049a2a
StriveTo <1101532319@qq.com>
2018-01-30 19:43:59 +0800

    add new vue

---
+++ ttgy/src/components/bannern.vue
@@ -0,0 +1,64 @@
+<template>
+	<section>
+		<div class="bannerN">
+			<div v-for="item in banner" @click="gosub()">
+				<img :src="item.image"/>
+			</div>
+		</div>
+		<div class="banner1">
+			<img :src="banner1.image"/>
+		</div>
+	</section>
+</template>
+
+<script>
+import axios from 'axios'
+import Swiper from 'swiper'
+
+export default {
+  	name: 'bannern',
+  	data: function() {
+    return {
+    		banner : [],
+    		banner1 : {}
+    	}
+  	},
+  	mounted() {
+  		axios.get("/v3/ad/homepage?connect_id=f1kfu0jn77q7qblifaod06hi70&type=0&lonlat=116.25153%2C40.11623&ad_code=110114&tab_id=")
+  		.then((res) => {
+  			console.log(res);
+  			this.banner = res.data.data.banner.mainBanners[2].content;
+  			this.banner1 = res.data.data.banner.mainBanners[4].content[0];
+  			console.log(this.banner);
+  			console.log(this.banner1);
+  		})
+  	},
+  	methods:{
+  		gosub(){
+  			
+  		}
+  	}
+}
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+section{
+	display: flex;
+	flex-direction: column;
+	.bannerN{
+		background: white;
+		padding-bottom: 0.2rem;
+		display: flex;
+		div{
+			width: 50%;
+			padding: 0.1rem 0.025rem 0;
+			img{
+				width: 100%;
+			}
+		}
+	}
+	.banner1 img{
+		width: 100%;
+	}
+}
+</style>
---
+++ ttgy/src/components/footer.vue
@@ -0,0 +1,58 @@
+<template>
+	<footer>
+		<div>
+			<i class="iconfont icon-home"></i>
+			<span>首页</span>
+		</div>
+		<div>
+			<i class="iconfont icon-fenlei1"></i>
+			<span>分类</span>
+		</div>
+		<div>
+			<i class="iconfont icon-gouwuche1"></i>
+			<span>购物车</span>
+		</div>
+		<div>
+			<i class="iconfont icon-icongerenzhongxin"></i>
+			<span>我的果园</span>
+		</div>
+	</footer>
+</template>
+
+<script>
+export default {
+  name: 'common-footer',
+  data () {
+    return {
+      	
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+	footer{
+		height: 0.42rem;
+		box-shadow: 0 -2px 2px 0 rgba(0,0,0,.15);
+		background: white;
+		font-size: 12px;
+		color: #898989;
+		display: flex;
+		justify-content: space-around;
+		padding-top: 0.03rem;
+		div{
+			display: flex;
+			flex-direction: column;
+			text-align: center;
+			flex: 1;
+			i{
+				font-size: 20px !important;
+			}
+			
+		}
+		div:first-of-type{
+			color: #65a032;
+		}
+	}
+</style>
