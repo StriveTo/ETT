b2087d4aa28da00f6c474f3a0c4378e5ffd98533
lpz10 <li2012670869@163.com>
2018-01-31 22:38:16 +0800

    66

--- ttgy/src/components/log.vue
+++ ttgy/src/components/log.vue
@@ -7,13 +7,13 @@
 		<div class="log_min">
 			<div class="user"> 
 				<b class="iconfont icon-icongerenzhongxin"></b>
-				<input type="text" placeholder="手机号" name="phone" maxlength="13" id="phone"/>
+				<input @input="changephone" type="text" placeholder="手机号" name="phone" maxlength="11" id="phone"/>
 				<i>X</i>
 				<span>获取验证码</span>
 			</div>
 			<div class="user"> 
 				<b class="iconfont icon-duigou"></b>
-				<input type="text" name="code" id="code" maxlength="6" placeholder="验证码" />
+				<input @input="changecode" type="text" name="code" id="code" maxlength="6" placeholder="验证码" />
 				<i>X</i>
 			</div>
 			<p>首次用手机号登录将自动为您注册，并有好礼相送</p>
@@ -46,6 +46,11 @@
 	import axios from "axios"
 	export default{
 		name:"log",
+		data(){
+			return{
+				inOn : $(".log_header input").val()
+			}
+		},
 		methods:{
 			changeLog:function(){
 				$(".log_min").css("right","100%");
@@ -56,13 +61,37 @@
 				$(".log_min").css("right","0");
 				$(".log_min2").css("right","-100%");
 				$(".log_header span").html("手机号快捷登录")
+			},
+			changephone:function(){
+				var phone = $("#phone").val();
+				if(phone.length>0){
+					$("#phone").siblings("i").css("display","inline-block");
+					if(phone.length == 11){
+						$("#phone").siblings("span").css("background","#f93");
+					}else{
+						$("#phone").siblings("span").css("background","#bfbfbf");
+					}
+				}else{
+					$("#phone").siblings("i").css("display","none");
+				}
+			},
+			changecode:function(){
+				var code = $("#code").val();
+				var phone = $("#phone").val();
+				if(code.length>0){
+					$("#code").siblings("i").css("display","inline-block");
+					if(code.length == 6&& phone.length == 11){
+						$(".log_min button").css("background","#f93")
+					}else{
+						$(".log_min button").css("background","#bfbfbf")
+					}
+				}else{
+					$("#code").siblings("i").css("display","none");
+				}
 			}
 		}
 	}
-	
-	
 </script>
-
 <style lang="scss">
 	@import "../assets/css/log.scss";
 </style>
--- ttgy/src/main.js
+++ ttgy/src/main.js
@@ -3,6 +3,11 @@
 import Vue from 'vue'
 import App from './App'
 
+import MintUI from 'mint-ui'
+import 'mint-ui/lib/style.css'
+
+Vue.use(MintUI)
+
 import router from './router'
 import footer from './components/footer'
 
